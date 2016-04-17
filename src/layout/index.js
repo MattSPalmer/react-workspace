34b00db31ef8e6516d42bc4ea0818ab0f94c8259
Matt Palmer <palmer.s.matt@gmail.com>
2016-04-17 19:07:49 -0400

    Add lifecycle methods and recalculate dimensions on resize

--- src/layout/index.js
+++ src/layout/index.js
@@ -1,4 +1,6 @@
 import React from 'react'
+import {findDOMNode} from 'react-dom'
+import {debounce} from 'lodash'
 import Pane from './pane'
 import {walkConfig} from './utility'
 
@@ -18,15 +20,33 @@ const Layout = React.createClass({
     const {config} = this.state
     return walkConfig(config)
   },
-  createConfig() {
-    const content = React.Children.map(this.props.children, c => {
+  createConfig(props = this.props) {
+    const content = React.Children.map(props.children, c => {
       if (c.type.createConfig)
         return c.type.createConfig(c)
     })
     return {content}
   },
+  updateDimensions() {
+    const elem = findDOMNode(this)
+    const {offsetWidth, offsetHeight} = elem
+    this.setState({parentDim: {w: offsetWidth, h: offsetHeight}})
+  },
+  handleResize() {
+    return debounce(this.updateDimensions, 50)()
+  },
   componentWillMount() {
     this.setState({config: this.createConfig()})
+  },
+  componentDidMount() {
+    window.addEventListener('resize', this.handleResize)
+    this.updateDimensions()
+  },
+  componentWillReceiveProps(nextProps) {
+    this.setState({config: this.createConfig(nextProps)})
+  },
+  componentWillUnmount() {
+    window.removeEventListener('resize', this.handleResize)
   },
   render() {
     return (
