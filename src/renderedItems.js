621b1040b7c9d66150ccd46bd9241cc819bb01b3
Matt Palmer <palmer.s.matt@gmail.com>
2016-09-03 15:33:07 -0400

    Apply React codemod pure-component to renderedItems.js.
    https://github.com/reactjs/react-codemod#pure-component

--- src/renderedItems.js
+++ src/renderedItems.js
@@ -1,78 +1,66 @@
 import React from 'react'
 
-class LayoutWrapper extends React.Component {
-  render() {
-    let {style, children} = this.props
-    style = {
-      overflow: 'hidden',
-      ...style,
-    }
-    return <div style={style}>{children}</div>
+function LayoutWrapper(props) {
+  let {style, children} = props
+  style = {
+    overflow: 'hidden',
+    ...style,
   }
+  return <div style={style}>{children}</div>
 }
 
-export class LayoutParent extends React.Component {
-  static propTypes = {
-    type: React.PropTypes.oneOf(['row', 'column']).isRequired
-  };
+export function LayoutParent(props) {
+  const {children, type} = props
+  const layoutStyle = {
+    display: 'flex',
+    flexDirection: type,
+    flexWrap: 'nowrap',
+    flexBasis: 'fit-content',
+  }
+  return (
+    <LayoutWrapper style={{...layoutStyle}}>
+      {children}
+    </LayoutWrapper>
+  )
+}
 
-  render() {
-    const {children, type} = this.props
-    const layoutStyle = {
-      display: 'flex',
-      flexDirection: type,
-      flexWrap: 'nowrap',
-      flexBasis: 'fit-content',
-    }
+export function LayoutItem(props) {
+  const Comp = props.componentClass
+  const Wrapper = props.wrapper
+  const {width, height} = props
+  const style = {
+    width, height,
+    flexGrow: 1,
+  }
+  return (
+    <LayoutWrapper style={style}>
+      <Wrapper {...props}>
+        <Comp {...props.innerProps} meta={props}/>
+      </Wrapper>
+    </LayoutWrapper>
+  )
+}
+
+export function NotFoundInRegister(props) {
+  const {meta} = props
+  const {width, height} = meta
+  if (process.env.NODE_ENV !== 'production') {
     return (
-      <LayoutWrapper style={{...layoutStyle}}>
-        {children}
-      </LayoutWrapper>
-    )
-  }
-}
-
-export class LayoutItem extends React.Component {
-  render() {
-    const Comp = this.props.componentClass
-    const Wrapper = this.props.wrapper
-    const {width, height} = this.props
-    const style = {
-      width, height,
-      flexGrow: 1,
-    }
-    return (
-      <LayoutWrapper style={style}>
-        <Wrapper {...this.props}>
-          <Comp {...this.props.innerProps} meta={this.props}/>
-        </Wrapper>
-      </LayoutWrapper>
-    )
-  }
-}
-
-export class NotFoundInRegister extends React.Component {
-  render() {
-    const {meta} = this.props
-    const {width, height} = meta
-    if (process.env.NODE_ENV !== 'production') {
-      return (
-        <div style={{width, height, fontSize: 16, overflow: 'scroll'}}>
-          <h1>Component '{meta.component}' not found...</h1>
-          <div>
-            <span>Here's what we know:</span>
-            <pre style={{margin: 0}}>
-              {JSON.stringify(this.props, null, 2)}
-            </pre>
-          </div>
-        </div>
-      )
-    } else {
-      return (
+      <div style={{width, height, fontSize: 16, overflow: 'scroll'}}>
+        <h1>Component '{meta.component}' not found...</h1>
         <div>
-          <pre>There was a problem loading this frame.</pre>
+          <span>Here's what we know:</span>
+          <pre style={{margin: 0}}>
+            {JSON.stringify(props, null, 2)}
+          </pre>
         </div>
-      )
-    }
+      </div>
+    )
+  } else {
+    return (
+      <div>
+        <pre>There was a problem loading this frame.</pre>
+      </div>
+    )
   }
 }
